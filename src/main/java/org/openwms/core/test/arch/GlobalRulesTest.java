cecc9b57096b4dd6aca4765ed754617a604805da
Heiko Scherrer <scherrer@openwms.org>
2024-07-19 22:26:18 +0100

    Initial commit

---
+++ src/main/java/org/openwms/core/test/arch/GlobalRulesTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2005-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openwms.core.test.arch;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
+
+/**
+ * A GlobalRules.
+ *
+ * @author Heiko Scherrer
+ */
+@AnalyzeClasses(packages = "org.openwms")
+public class GlobalRulesTest {
+
+    @ArchTest
+    public static final ArchRule slicesFreeOfCycles = slices().matching("org.openwms.(*)..").should().beFreeOfCycles();
+
+}
